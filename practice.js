684333c6b91d5511fc6d716bb1fbc093c22f4a68
sai <jayasaivardhan.jv@gmail.com>
2022-09-27 10:16:38 +0530

    new code for sai

--- practice.js
+++ practice.js
@@ -1,9 +1,1 @@
-console.log("first line of practice");
-
-console.log("sai's code");
-
-console.log("sai third line");
-
-console.log("sai beanch is created");
-
-console.log("final line of code");
+console.log("sai is a good boy");
